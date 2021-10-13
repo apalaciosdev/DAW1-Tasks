ae6b8f285210af5b5d40a2bf6533419d8c4dd8a3
Aaron Palacios Olea <apalacios.dev@gmail.com>
2021-10-13 19:13:03 +0200

    131021

--- M03/UF1/pt2/src/Ex1.java
+++ M03/UF1/pt2/src/Ex1.java
@@ -4,13 +4,13 @@ public class Ex1 {
     public static void main(String[] args) throws Exception {
         // EJERCICIO 1
         
-        Scanner tecladoEj1 = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduce el primer número:");
-        int numero1Ej1 = tecladoEj1.nextInt();
+        int numero1Ej1 = teclado.nextInt();
 
         System.out.println("Introduce el segundo número:");
-        int numero2Ej2 = tecladoEj1.nextInt();
+        int numero2Ej2 = teclado.nextInt();
 
         System.out.println("El resultado sumando los números es: " + (numero1Ej1 + numero2Ej2));
         System.out.println("El resultado restando los números es: " + (numero1Ej1 - numero2Ej2));
@@ -22,15 +22,35 @@ public class Ex1 {
 
 
         //EJERCICIO 2
-        Scanner tecladoEj2 = new Scanner(System.in);
 
         System.out.println("Introduce la temperatura en grados Fahrenheit:");
-        int numeroTemperaturaEj2 = tecladoEj2.nextInt();
-        float calculo = ((numeroTemperaturaEj2-32)*5/9);
+        int numeroTemperaturaEj2 = teclado.nextInt();
+        float calculo = (float) ((numeroTemperaturaEj2-32)*5/9);
         
         System.out.printf(numeroTemperaturaEj2 + "F son %.2f ºC\n", calculo);
 
         
+
+
+
+        //EJERCICIO 3
+
+        System.out.println("Introduce los Km recorridos:");
+        int kilometrosEj3 = teclado.nextInt();
+        float calculoMetrosEj3 = (float) kilometrosEj3 * 1000;
+
+        System.out.println("Introduce el tiempo (en horas) que has tardado:");
+        int tiempoHorasEj3 = teclado.nextInt();
+
+
+        System.out.printf("El coche ha hecho " + kilometrosEj3 + "Km en " + tiempoHorasEj3 + "horas \n");
+        System.out.printf("En una hora a hecho %.2f metros", (calculoMetrosEj3/(float)tiempoHorasEj3));
+
+
+
+       
+
+
 
 
 
