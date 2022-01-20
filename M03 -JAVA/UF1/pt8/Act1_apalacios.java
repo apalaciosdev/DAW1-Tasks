a5904d33ad2f4610d951698928d0992685f74fbe
apalacios <apalacios.dev@gmail.com>
2022-01-20 14:16:27 +0100

    2201201416

---
+++ M03 -JAVA/UF1/pt8/Act1_apalacios.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.lang.*;
+
+public class Act1_apalacios {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in, "UTF-8");   
+        
+        ArrayList<String> pacientes = new ArrayList<String>();
+        boolean state = true;
+        int pacientesRegistrados = 0;
+        
+        
+        for(int i = 0; i < 4; i++){
+            System.out.print("Introduce el nombre del paciente "+ (i+1) + ":");
+            String nombrePaciente = teclado.nextLine();
+            pacientes.add(nombrePaciente);
+            pacientesRegistrados ++;
+        }
+        
+        
+        
+        while(state && pacientesRegistrados < 10){
+            
+            //Medico trata a paciente
+            System.out.print("La doctora cura al paciente "+ pacientes.remove(0) + "\n");
+            
+            System.out.print("Introduce el nombre del siguiente paciente:");
+            String nombrePaciente = teclado.nextLine();
+            pacientesRegistrados ++;
+
+            
+            //Si no se introduce nada en el input
+            if(nombrePaciente.equalsIgnoreCase("")){
+                System.out.println("La jornada termina por hoy. La doctora tratará al resto de pacientes.");
+                state = false;
+            }
+
+
+            //Colar pacientes
+            System.out.print("¿Deseas colar al paciente? (s/n)");
+            String opt = teclado.nextLine().toLowerCase();
+            if(opt.equals("s")){
+                pacientes.add(0, nombrePaciente);
+            } else {
+                pacientes.add(nombrePaciente);
+            }
+            
+        }
+        
+        System.out.println("La jornada ha terminado por hoy. La doctora tratará al resto de pacientes.");
+        
+        
+    
+        
+
+
+        System.out.println("\n\n---------------- FIN DEL PROGRAMA ----------------");
+        System.out.println("----------------- Aaron Palacios -----------------");
+    }
+}
---
+++ M03 -JAVA/UF1/pt8/Act2_apalacios.java
@@ -0,0 +1,121 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class Act2_apalacios {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in);   
+        
+        ArrayList <ArrayList> arraylist1 = new ArrayList<>();
+      
+        boolean añadirAsistente = false;
+        int contadorPacientes = 0;
+        
+        do {
+            arraylist1.add(new ArrayList());
+            System.out.println("Introduce el nombre del asistente:");
+            String nombreAsistente = teclado.next();
+            arraylist1.get(contadorPacientes).add(nombreAsistente);
+            
+            
+            System.out.println("Introduce su edad:");
+            int edadAsistente = teclado.nextInt();
+            arraylist1.get(contadorPacientes).add(edadAsistente);
+            
+
+            System.out.println("Introduce los dias que lleva sin beber:");
+            int diasSequiaAsistente = teclado.nextInt();
+            arraylist1.get(contadorPacientes).add(diasSequiaAsistente);
+            
+            
+            System.out.println("Quieres añadir a alguien más? (true/false)");
+            añadirAsistente = teclado.nextBoolean();
+            contadorPacientes ++;
+        } while (añadirAsistente);
+        
+        
+        
+        System.out.println("\n\nEn total han asistido " + contadorPacientes + " personas.");
+        
+        for (int i = 0; i < arraylist1.size(); i++) {
+            System.out.println("\n####################");
+            System.out.println("Datos del paciente " + (i+1));
+            
+            for (int j = 0; j < 3; j++) {
+                String titulares[]={"NOMBRE: ", "EDAD: ", "DIAS SECO: "};
+                System.out.println(titulares[j] + arraylist1.get(i).get(j));
+            }
+            System.out.println("####################\n");
+        }
+        
+        
+        System.out.println("¿Deseas modificar la lista? (s/n)");
+        String modificarLista = teclado.next().toLowerCase();
+        
+        do {
+            
+            if(modificarLista.equals("s")){
+                System.out.println("¿Que asistente deseas modificar?");
+                int modificacionAsistente = (teclado.nextInt())-1;
+                
+
+                System.out.println("Introduce su nombre:");
+                String nombreAsistente = teclado.next();
+                arraylist1.get(modificacionAsistente).add(0, nombreAsistente);
+                
+                
+                System.out.println("Introduce su edad:");
+                int edadAsistente = teclado.nextInt();
+                arraylist1.get(modificacionAsistente).add(1, edadAsistente);
+                
+                
+                System.out.println("Introduce los dias que lleva sin beber:");
+                int diasSequiaAsistente = teclado.nextInt();
+                arraylist1.get(modificacionAsistente).add(2, diasSequiaAsistente);
+                
+
+                System.out.println("¿Deseas realizar otra modificación? (s/n)");
+                modificarLista = teclado.next().toLowerCase();
+            }
+            
+        } while (modificarLista.equals("s"));
+       
+       
+        System.out.println("¿Deseas borrar algun asistente de la lista? (s/n)");
+        String borrarAsistente = teclado.next().toLowerCase();
+        
+        do {
+            
+            if(borrarAsistente.equals("s")){
+                System.out.println("¿Que asistente deseas borrar?");
+                int asistenteEliminado = (teclado.nextInt())-1;
+                
+                arraylist1.remove(asistenteEliminado);
+
+                System.out.println("¿Deseas borrar algun asistente más? (s/n)");
+                borrarAsistente = teclado.next().toLowerCase();
+            }
+            
+        } while (borrarAsistente.equals("s"));
+        
+        
+        
+        System.out.println("\n\nEn total han asistido " + contadorPacientes + " personas.");
+        
+        for (int i = 0; i < arraylist1.size(); i++) {
+            System.out.println("\n####################");
+            System.out.println("Datos del paciente " + (i+1));
+            
+            for (int j = 0; j < 3; j++) {
+                String titulares[]={"NOMBRE: ", "EDAD: ", "DIAS SECO: "};
+                System.out.println(titulares[j] + arraylist1.get(i).get(j));
+            }
+            System.out.println("####################\n");
+        }
+
+
+        System.out.println("\n\n---------------- FIN DEL PROGRAMA ----------------");
+        System.out.println("----------------- Aaron Palacios -----------------");
+    }
+}
