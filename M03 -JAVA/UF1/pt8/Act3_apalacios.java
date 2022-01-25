96cc3e2deb03d9640f2b0cf96bd0381c9eb7bd1a
apalacios <apalacios.dev@gmail.com>
2022-01-25 16:17:12 +0100

    2201251617

---
+++ M03 -JAVA/UF1/pt8/Act3_apalacios.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+
+public class Act2_apalacios {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in);   
+        
+        ArrayList <ArrayList> arraylist1 = new ArrayList<>();
+      
+            
+        //idexOf()
+
+
+        System.out.println("\n\n---------------- FIN DEL PROGRAMA ----------------");
+        System.out.println("----------------- Aaron Palacios -----------------");
+    }
+}
