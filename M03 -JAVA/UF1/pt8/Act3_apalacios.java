efa5a0a86f83156cd9b9ed1914bd8ae8f9a96a0c
apalacios <apalacios.dev@gmail.com>
2022-01-26 17:13:16 +0100

    2201261713

--- M03 -JAVA/UF1/pt8/Act3_apalacios.java
+++ M03 -JAVA/UF1/pt8/Act3_apalacios.java
@@ -50,25 +50,115 @@ public class Act3_apalacios {
                 }
             } 
         }
-
+        state=true;
+        
         
 
-        //comprobamos si está Manel
-        if(alumnos.contains("Manel")){
-            System.out.println("Manel está en la posición " + alumnos.indexOf("Manel"));
+        //comprobamos si un alumno está en la lista
+        while(state){
+            System.out.println("\n\n¿Deseas comprobar si algún alumno está en la lista? (s/n)");
+            String eleccion = teclado.nextLine().toLowerCase();
+           
+            while(eleccion.equals("s")){
+                System.out.println("Introduce el nombre del alumno:");
+                String alumno = teclado.nextLine();
+                
+                if(alumno.equalsIgnoreCase("")) {  eleccion="n"; }
+
+                else{
+                    if(alumnos.contains(alumno)){
+                        System.out.println(alumno + " está inscrito en la posición " + alumnos.indexOf(alumno));
+                        eleccion="n";
+                    }
+    
+                    else{
+                        System.out.println("El alumno introducido no existe o no está en la lista. Prueba de nuevo o deja en blanco para salir.\n");
+                    }
+                }
+            }
+            state=false;
         }
+        state=true;
+    
+      
 
-        //añadimos a Maria y mostramos la lista de alumnos
-        alumnos.add("Maria");
-        for (String nombreAlumno : alumnos) {
-            System.out.println(nombreAlumno);
+        //introducimos a un alumno que se ha inscrito a última hora
+        System.out.println("\n\n¿Deseas añadir a algún alumno en la lista? (s/n)");
+        String eleccionInscribir = teclado.nextLine().toLowerCase();
+    
+        if(eleccionInscribir.equals("s")){
+            System.out.println("Introduce el nombre del alumno:");
+            String alumno = teclado.nextLine();
+            
+            alumnos.add(alumno);
+            System.out.println("Nombres de los alumnos: " );
+            for (String nombreAlumno : alumnos) {
+                System.out.println(nombreAlumno);
+            }
         }
+    
+      
 
+        //sustituimos a un alumno enfermo por uno nuevo
+        System.out.println("¿Deseas sustituir a algún alumno enfermo? (s/n)");
+        String eleccionEnfermo = teclado.nextLine().toLowerCase();
+    
+        if(eleccionEnfermo.equals("s")){
+            System.out.println("Introduce el nombre del alumno enfermo:");
+            String alumnoEnfermo = teclado.nextLine();
 
+            if(alumnos.contains(alumnoEnfermo)){
+                System.out.println("Introduce el nombre del nuevo alumno:");
+                String nuevoAlumno = teclado.nextLine();
 
+                int posicionAlumnoEnfermo = alumnos.indexOf(alumnoEnfermo);
+                alumnos.set(posicionAlumnoEnfermo, nuevoAlumno);
+
+            }
+
+            System.out.println("Nombres de los alumnos: " );
+            for (String nombreAlumno : alumnos) {
+                System.out.println(nombreAlumno);
+            }
+        }
+ 
+     
+
+        //comprobamos si vamos al Camp Nou
+        if(lugares.contains("Camp Nou")){
+            System.out.println("Iremos al Camp Nou");
+
+            System.out.println("Lugares que visitaremos: " );
+            for (String lugar : lugares) {
+                System.out.println(lugar);
+            }
+        }
         
-        //indexOf()
-        //contains()
+        else{
+            System.out.println("Al principio no ibamos a ir al Camp Nou, pero ahora sí.");
+            lugares.add(1, "Camp Nou");
+
+            System.out.println("Lugares que visitaremos: " );
+            for (String lugar : lugares) {
+                System.out.println(lugar);
+            }
+        }
+
+
+
+        //borrar el Museo Picasso si existe
+        if(lugares.contains("Museo Picasso")){
+            System.out.println("No iremos al Museo Picasso por obras");
+
+            int posicionMuseo = lugares.indexOf("Museo Picasso");
+            lugares.remove(posicionMuseo);
+
+            System.out.println("Lugares que visitaremos: " );
+            for (String lugar : lugares) {
+                System.out.println(lugar);
+            }
+        }
+        
 
         System.out.println("\n\n---------------- FIN DEL PROGRAMA ----------------");
         System.out.println("----------------- Aaron Palacios -----------------");
