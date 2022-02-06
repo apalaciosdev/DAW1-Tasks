ec48f4faa889a226aea47486568553e804e59e27
apalacios <apalacios.dev@gmail.com>
2022-02-06 18:12:19 +0100

    2202061812

--- M03 -JAVA/UF2/pt1/Act1_apalacios.java
+++ M03 -JAVA/UF2/pt1/Act1_apalacios.java
@@ -6,21 +6,19 @@ public class Act1_apalacios {
 
         Scanner teclado = new Scanner(System.in, "UTF-8");   
         float maxKmCoches[] = {};
-        
+
         System.out.println("Introduce número de modelos de coche que dispones:");
         int cantidadModelos = teclado.nextInt();
-        
+
         for (int i = 0; i < cantidadModelos; i++) {
             System.out.println("MODELO " + (i+1));
             System.out.println("¿Cual es la distancia máxima en KM que permite recorrer diáriamente?");
             float kmMaxDiarios = teclado.nextFloat();
-            
+
             maxKmCoches[i] = kmMaxDiarios;
-            
+
         }
-        
-    
-        
+
 
 
         System.out.println("\n\n---------------- FIN DEL PROGRAMA ----------------");
