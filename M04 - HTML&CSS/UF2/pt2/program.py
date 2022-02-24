3bbf13ca5af3ec396e3fd3f6dc4032c8d14e6018
apalacios <apalacios.dev@gmail.com>
2022-02-24 11:24:22 +0100

    2202241124

--- M04 - HTML&CSS/UF2/pt2/program.py
+++ M04 - HTML&CSS/UF2/pt2/program.py
@@ -59,22 +59,35 @@ class ExistDBTest():
         
         qy2 =  "for $prod in /productos/produc where cod_zona='20'return $prod"
         qres2 = self.db.query(qy2)
-   
-        for x in range(0,len(qres2.results)):
+    
+        for x in range(0, 2):
             #Recogemos todos los campos de los productos con cod_zona 20. Lo hago en un bucle por si hay más de un producto
-            codProd =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $codProd:= data($prod/cod_prod) return $codProd")
-            denominacion =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $denominacion:= data($prod/denominacion) return $denominacion")
-            precio =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $precio:= data($prod/precio) return $precio")
-            stockActual =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $stockActual:= data($prod/stock_actual) return $stockActual")
-            stockMinimo =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $stockMinimo:= data($prod/stock_minimo) return $stockMinimo")
-            stock =  int(str(stockActual)) - int(str(stockMinimo))
+            codProdQuery = self.db.query("for $prod in /productos/produc where cod_zona='20' let $codProd:= data($prod/cod_prod) return $codProd")
+            codProd = str(codProdQuery).split()
             
+            denominacionQuery =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $denominacion:= data($prod/denominacion) return $denominacion")
+            denominacion = str(denominacionQuery).split()
+            
+            precioQuery =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $precio:= data($prod/precio) return $precio")
+            precio = str(precioQuery).split()
+            
+            stockActualQuery =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $stockActual:= data($prod/stock_actual) return $stockActual")
+            stockActual = str(stockActualQuery).split()
+            
+            stockMinimoQuery =  self.db.query("for $prod in /productos/produc where cod_zona='20' let $stockMinimo:= data($prod/stock_minimo) return $stockMinimo")
+            stockMinimo = str(stockMinimoQuery).split()
+            
+            stock =  int(stockActual[x]) - int(stockMinimo[x])
+            
+           
             
             #info de zonas
             nombreZona =  self.db.query("for $zona in /zonas/zona where cod_zona='20' let $nombre:= data($zona/nombre) return $nombre")
-            directorZona =  self.db.query("for $zona in /zonas/zona where cod_zona='20' let $director:= data($zona/director) return $director")
             
-            insertData =  "update insert <producto><cod_prod>" + str(codProd) + "</cod_prod><denominacion>" + str(denominacion) + "</denominacion><precio>" + str(precio) + "</precio><nombre_zona>" + str(nombreZona) + "</nombre_zona><director>" + str(directorZona) + "</direct><stock>" + str(stock) + "</stock></producto> into /zones20"
+            directorZona =  self.db.query("for $zona in /zonas/zona where cod_zona='20' let $director:= data($zona/director) return $director")
+ 
+            
+            insertData =  "update insert <producto><cod_prod>" + str(codProd[x]) + "</cod_prod><denominacion>" + str(denominacion[x]) + "</denominacion><precio>" + str(precio[x]) + "</precio><nombre_zona>" + str(nombreZona) + "</nombre_zona><director>" + str(directorZona) + "</director><stock>" + str(stock) + "</stock></producto> into /zones20"
             self.db.query(insertData)
            
             # print(directorZona)
