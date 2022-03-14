9c4e714772d3ed48d6e19cfbc9be44a7b33cb333
apalacios <apalacios.dev@gmail.com>
2022-03-14 19:39:59 +0100

    2203141939

--- M04 - HTML&CSS/UF2/pt4/program.py
+++ M04 - HTML&CSS/UF2/pt4/program.py
@@ -2,15 +2,16 @@ from pymongo import MongoClient
 import json
 from tabulate import tabulate
 
-client = MongoClient("mongodb://localhost:27017")
-db = client.deps
+
+conn = MongoClient("mongodb://localhost:27017")
+db = conn.deps
+collection = db.deps
 
 
 def menu():
   while True:  # Mini menú para escoger que función hacer
-    print(
-      "1.- Ver empleados\n",
-      "2.- Eliminar una película\n",
+    print("1.- Ver empleados\n",
+      "2.- Insertar un departamento\n",
       "3.- Hacer migración de archivos\n",
       "4.- Mostrar información de las películas categorizadas por nacionalidad",
     )
@@ -22,7 +23,7 @@ def menu():
       break
 
     elif choice == 2:
-      # deletePelicula()
+      insertaDep()
       break
 
     elif choice == 3:
@@ -40,7 +41,7 @@ def menu():
 
 
 def verEmple():
-  dep = input("Teclea departamento: ")
+  dep = input("Introduce el nuevo departamento: ")
   
   f=open('empleats.json')
   empjson=json.loads(f.read())
@@ -55,7 +56,44 @@ def verEmple():
       append = [i['EMP_NO'], i['APELLIDO'], i['OFICIO'], i['DIR'], i['FECHA_ALT'], i['SALARIO'], i['DEPT_NO']]
       table.append(append)
 
-
   print(tabulate(table, headers='firstrow', tablefmt='fancy_grid', showindex='always'))
+  
+
+
+
+def insertaDep():
+  dep = input("Introduce el número del nuevo departamento: ")
+  depName = input("Introduce el nombre del nuevo departamento: ")
+  depDir = input("Introduce la dirección del nuevo departamento: ")
+  checkedDeps = []
+  
+  f=open('empleats.json')
+  empjson=json.loads(f.read())
+  f.close()
+  
+  for i in empjson['EMPLEADOS']['EMP_ROW']:
+    if(i['DEPT_NO'] in checkedDeps): # Comprobamos si el genero ya se ha utilizado
+      pass
+    
+    else:
+      checkedDeps.append(i['DEPT_NO'])
+    
+    
+    
+  if(i['DEPT_NO'] == dep):
+    print("Ya existe un departamento con este código: ", dep)
+  
+  else:
+    newEntry = {
+      "name": dep,
+      "eid": depName,
+      "location": depDir
+    }
+      
+    # Insert Data
+    collection.insert_one(newEntry)
+    
+    
+
 
 menu()
