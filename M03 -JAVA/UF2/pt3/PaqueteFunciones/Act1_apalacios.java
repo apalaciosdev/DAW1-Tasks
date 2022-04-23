478a34dc018ea73d734b853a28dbedcbb924de8e
apalaciosdev <apalacios.dev@gmail.com>
2022-04-23 15:50:01 +0200

    2204231549

--- M03 -JAVA/UF2/pt3/PaqueteFunciones/Act1_apalacios.java
+++ M03 -JAVA/UF2/pt3/PaqueteFunciones/Act1_apalacios.java
@@ -25,24 +25,16 @@ public class Act1_apalacios {
             num = sc.nextInt();
             
       
-            if (num == 1) {
-                saltoLongitud();
+            if (num == 1 || num == 2 || num == 3) {
+                saltosMain(num);
             }
-      
-            else if (num == 2) {
-                // saltoAltura();
-            }
-      
-            else if (num == 3) {
-                // saltoPertiga();
-            }
-      
+
             else if (num == 4) {
                 bucle = false;
             }
             
             else {
-                System.out.println("OPCIÓN NO DISPONIBLE, INTENTALO DE NUEVO");
+                System.out.println("OPCIÓN NO VÁLIDA, INTENTALO DE NUEVO");
             }
         }
 
@@ -51,7 +43,27 @@ public class Act1_apalacios {
     }
 
     
-    public static String[] participantes(){
+    
+    
+    
+
+    
+    
+    public static void saltosMain(int opcionSalto){
+        int numeroParticipantes = numeroParticipantes();
+        String arrayParticipantes[] = añadirNombresParticipantes(numeroParticipantes);
+
+        float arraySaltos[][] = hacerSaltos(numeroParticipantes, arrayParticipantes, opcionSalto);
+        
+       
+
+
+        // float sis = funcionSalto.crearRandom(1);
+        // System.out.println(sis);
+    }
+
+
+    public static int numeroParticipantes(){
         
         int cantidadParticipantes = 0;
         
@@ -73,30 +85,83 @@ public class Act1_apalacios {
             }
         }
         
+        return cantidadParticipantes;
+    }
 
+
+    public static String[] añadirNombresParticipantes(int cantidadParticipantes){
         String arrayParticipantes[] = new String[cantidadParticipantes];
 
         for (int i = 0; i < arrayParticipantes.length; i++) {
             System.out.println("\nIntroduce el nombre del participante " + (i+1));
             arrayParticipantes[i] = sc.next();
         }
-        
+
         return arrayParticipantes;
     }
-    
-    
-    
 
 
+    public static float[][] hacerSaltos(int numeroParticipantes, String[] nombresParticipantes, int opcionSalto){
+        float saltosParticipantes[][] = new float[numeroParticipantes][3];
+        boolean statePrimerPart = true;
+        boolean statePrimerPart2 = true;
+        float saltoParticipante = 0;
+        float validOrNot = 0;
+        
+        for (int i = 0; i < numeroParticipantes; i++) {
+            System.out.println("\n\nTurno del participante " + (i+1));
+            for (int j = 0; j < 3; j++) {
+                // Salto válido o no válido
+                validOrNot = funcionSalto.crearRandom(4);
+                
+                if(validOrNot == 1){ //Si el salto es válido
+                    
+                    // Primer participante
+                    while (statePrimerPart) {
+
+                        if(i == 0){
+                            while (statePrimerPart2) {
+                                try {
+                                    Scanner sc = new Scanner(System.in);
+                                    System.out.println("Introduce la distáncia del salto realizado: ");
+                                    saltoParticipante = sc.nextFloat();
+                                    System.out.print("\n");
+                                    statePrimerPart2 = false;
+                                    
+                                } catch (InputMismatchException excep) {
+                                    System.out.println("\n-------------------------------------");
+                                    System.out.println("Error, solo puedes introducir números");
+                                    System.out.println("-------------------------------------\n");
+                    
+                                }
+                            }
+                        }
     
-    public static void saltoLongitud(){
-        String arrayParticipantes [] = participantes();
-        for (String string : arrayParticipantes) {
-            System.out.println(string);
+                        String validarSalto = funcionSalto.checkIsValid(opcionSalto, saltoParticipante);
+                        
+                        if(validarSalto.equals("ok")){
+                            statePrimerPart = false;
+                        }
+    
+                        else{
+                            System.out.println(validarSalto + "\n\n");
+                            statePrimerPart2 = true;
+                        }
+                    }
+     
+
+                    // Resto de participantes
+                }
+
+                else{ //Si el salto no es válido
+                    System.out.println("\nSalto " + (i+1) + "no válido \n");
+                }
+
+
+            }
         }
 
-        float sis = funcionSalto.crearRandom(1);
-        System.out.println(sis);
+        return saltosParticipantes;
     }
 
 
--- M03 -JAVA/UF2/pt3/PaqueteFunciones/funcionesSaltos.java
+++ M03 -JAVA/UF2/pt3/PaqueteFunciones/funcionesSaltos.java
@@ -1,27 +1,60 @@
 package PaqueteFunciones;
 
 public class funcionesSaltos {
-  public static float crearRandom(int tipo){
-    float randomNumber = 0;
-    switch (tipo) {
-        case 1:
-            randomNumber = (float) ((float)Math.floor(Math.random() * (9 - 1 + 1)))+1;
-            break;
+
+    public static float crearRandom(int tipo) {
+        float randomNumber = 0;
+        switch (tipo) {
+            case 1: //Salto longitud
+                randomNumber = (float) ((float) Math.floor(Math.random() * (9 - 1 + 1))) + 1;
+                break;
+
+            case 2: //Salto altura
+                randomNumber = (float) ((float) Math.floor(Math.random() * (1.5 - 0.8 + 1)) + 0.8);
+                break;
+
+            case 3: //Salto pértiga
+                randomNumber = (float) ((float) Math.floor(Math.random() * (6.3 - 3 + 1)) + 3);
+                break;
             
-            case 2:
-            randomNumber = (float) ((float) Math.floor(Math.random() * (1.5 - 0.8 + 1)) + 0.8);
-            break;
-            
-        case 3:
-            randomNumber = (float) ((float) Math.floor(Math.random() * (6.3 - 3 + 1)) + 3);    
-            break;
-    
-        default:
-            break;
+            case 4: //Salto válido o no válido
+                randomNumber = (float) ((float) Math.floor(Math.random() * (1 - 0 + 1)) + 0);
+                break;
+
+            default:
+                break;
+        }
+
+        return randomNumber;
     }
+    
 
-    return randomNumber;
-}
+    public static String checkIsValid(int tipo, float saltoIntroducido) {
+        String message = "ok";
+        switch (tipo) {
+            case 1: //Salto longitud
+                if(saltoIntroducido < 1 || saltoIntroducido > 9){
+                    message = "Introduce un número del 1 al 9";
+                }
+                break;
+                
+            case 2: //Salto altura
+                if(saltoIntroducido < 0.8 || saltoIntroducido > 1.5){
+                    message = "Introduce un número del 0.8 al 1.5";
+                }
+                break;
+                
+            case 3: //Salto pértiga
+                if(saltoIntroducido < 3 || saltoIntroducido > 6.3){
+                    message = "Introduce un número del 3 al 6.3";
+                }
+                break;
+            
+            default:
+                break;
+        }
 
+        return message;
+    }
 
 }
