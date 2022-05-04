d8d98721cd4c09f8b10e9437b1f888b8aae2d8e9
apalaciosdev <apalacios.dev@gmail.com>
2022-05-04 11:47:51 +0200

    2206041147

---
+++ M03 -JAVA/UF2/pt4/Act1_apalacios.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.File;
+
+public class Act1_apalacios {
+    public static Scanner sc = new Scanner(System.in, "UTF-8");
+
+    public static void main(String[] args) {
+
+        System.out.println("hola");
+
+    }
+}
---
+++ M03 -JAVA/UF3/act2/ActFicheros_apalacios.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.File;
+
+public class ActFicheros_apalacios {
+    public static Scanner sc = new Scanner(System.in, "UTF-8");
+
+    public static void main(String[] args) {
+
+        String filePath = "C:/Users/6002215/Downloads/Test";
+
+        File file = new File(filePath);
+
+        String path = file.getPath();
+
+        System.out.println(path);
+
+        String[] listado = file.list();
+
+        if (listado == null || listado.length == 0) {
+
+            System.out.println("No hay elementos dentro de la carpeta actual");
+
+            return;
+
+        }
+
+        else {
+
+            for (int i = 0; i < listado.length; i++) {
+
+                System.out.println(listado[i]);
+
+            }
+
+        }
+
+    }
+}
